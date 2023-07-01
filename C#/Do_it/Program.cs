40cdfed0502a6680699bdf8a37fcabf965ebd39d
여지윤 <81175074+yeojiyoon@users.noreply.github.com>
2023-07-01 16:45:46 +0900

    TEST COMMIT

---
+++ C#/Do_it/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(String[] args)
+    {
+        Person P1 = new Person();
+        P1.Name = "서준";
+
+        P1.Eat();
+    }
+}
+
+class Person 
+{
+    public string Name = "";
+    public string Birthday = "";
+    public void Eat()
+    {
+        Console.WriteLine(Name + "이 밥을 먹습니다");
+    }
+}
