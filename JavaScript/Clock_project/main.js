d11230cbe9fbd475b4c8ea71846703b2bf07a65e
여지윤 <81175074+yeojiyoon@users.noreply.github.com>
2023-11-20 00:48:46 +0900

    ADD HOLY GRAIL LAYOUT AND ADD BUTTON IN CLOCK

--- JavaScript/Clock_project/main.js
+++ JavaScript/Clock_project/main.js
@@ -10,6 +10,28 @@ function setClock(){
     document.getElementById("date").innerHTML = year + "년 " + month + "월 " + date + "일";
 }
 
+function setClock2(){
+    var dateInfo = new Date();
+    var hour = modifyNumber(dateInfo.getHours());
+    var min = modifyNumber(dateInfo.getMinutes());
+    var sec = modifyNumber(dateInfo.getSeconds());
+    var year = dateInfo.getFullYear();
+    var month = dateInfo.getMonth();
+    var date = dateInfo.getDate();
+    var PA;
+
+    if(hour>=12) {
+        hour = hour - 12;
+        PA = "PM";
+    }
+    else {
+        PA = "AM";
+    }
+
+    document.getElementById("time").innerHTML = PA + " " + hour + ":" + min + ":" + sec;
+    document.getElementById("date").innerHTML = year + "년 " + month + "월 " + date + "일";
+}
+
 function modifyNumber(time){
     if(parseInt(time)<10){
         return "0" + time;
@@ -19,6 +41,20 @@ function modifyNumber(time){
 }
 
 window.onload = function(){
+    let interval1 = setInterval(setClock, 1000);
+    let interval2 = setInterval(setClock2, 1000);
+
     setClock();
-    setInterval(setClock, 1000);
+
+    document.getElementById("btn1").onclick = function(){
+        clearInterval(interval1);
+        setClock2();
+        interval2 = setInterval(setClock2, 1000);
+    }
+
+    document.getElementById("btn2").onclick = function(){
+        clearInterval(interval2);
+        setClock();
+        interval1 = setInterval(setClock, 1000);
+    }
 }
--- Web/README.md
+++ Web/README.md
@@ -1,1 +1,7 @@
-test
+## 겁나 빠른 웹 레시피 따라하기
+
+study period: 2023.10.30~
+
+---
+
+link: https://youtu.be/5ui9JmfdiUc?si=tDSgRF0yqTjLWWBg
